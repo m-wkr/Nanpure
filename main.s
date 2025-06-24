32d2ed446dd037e5daf7f61293250e7b9c9d0192
m-wkr <maylun.mw@outlook.com>
2025-06-24 17:00:19 -0400

    Provided basic user input reading

--- main.s
+++ main.s
@@ -3,16 +3,45 @@
 .include "printing.s"
 
 _start:
-    writeToScreen grid, #801
-_exit:
+    writeToScreen grid, #819
+
+    B obtainInput
+exit:
     MOV x0, #1
     MOV x8, #93
     SVC 0
 
+obtainInput: //name WIP
+    writeToScreen enterCoords, #54
+
+    MOV x0, #0
+    LDR x1, =userCoords 
+    MOV x2, #2
+    MOV x8, #63
+    SVC 0
+
+
+    writeToScreen userCoords, #2
+
+    writeToScreen enterValue, #46
+
+    MOV x0, #0
+    LDR x1, =userValue
+    MOV x2, #1
+    MOV x8, #63
+    SVC 0
+
+
+    writeToScreen userValue, #1
+
+    B exit
+
 .data
-    enterMessage: .asciz "Enter the grid coord for the number in the format yx: " //54 characters, excluding x0
-    grid: .asciz "
-(x position is horizontal, y position is vertical, both starting from 00 at the top left corner) 
+    enterCoords: .asciz "Enter the grid coord for the number in the format yx: " //54 characters, excluding x0
+    enterValue: .asciz "Enter the value at your provided coordinates: " //46 excl x0
+    userCoords: .fill 2,1,0
+    userValue: .fill 1,1,0
+    grid: .asciz "(x position is horizontal, y position is vertical, both starting from 00 at the top left corner)
  --- --- --- --- --- --- --- --- --- 
 |   |   |   |   |   |   |   |   |   |
  --- --- --- --- --- --- --- --- --- 
@@ -32,4 +61,4 @@ _exit:
  --- --- --- --- --- --- --- --- --- 
 |   |   |   |   |   |   |   |   |   |
  --- --- --- --- --- --- --- --- --- 
-"  //grid has 801 characters, 705 excluding tagline (x0 omitted)
+"  //grid has 819 (x0 omitted)
