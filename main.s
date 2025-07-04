ee5baa012de07ba6e12f0eb33d33647350e345ed
m-wkr <maylun.mw@outlook.com>
2025-07-04 17:01:09 -0400

    Introduced main looping mechanism

--- main.s
+++ main.s
@@ -3,10 +3,9 @@
 .include "IO.s"
 .include "gridManipulation.s"
 
-
-_start:
+.macro mainLoop
     writeToScreen gridStr, #819
-
+1:
     obtainInput
 
     insertValueToGrid
@@ -14,6 +13,32 @@ _start:
     insertValueToGridString
 
     writeToScreen gridStr, #819
+2:
+    writeToScreen confirmationMsg, #60
+3:
+    MOV x0, #0
+    LDR x1, =returnBuffer 
+    MOV x2, #1
+    MOV x8, #63
+    SVC 0
+4:
+    LDRB w3, [x1]
+    CMP x3, #89
+
+    SVC 0 //Read and move past return character
+
+    BEQ 1b
+
+    CMP x3, #121
+
+    BEQ 1b
+
+    CMP x3, #110
+    BNE 2b
+.endm
+
+_start:
+    mainLoop
 
 exit:
     MOV x0, #1
@@ -26,6 +51,8 @@ exit:
 
     enterCoords: .asciz "Enter the grid coord for the number in the format yx: " //54 characters, excluding x0
     enterValue: .asciz "Enter the value at your provided coordinates: " //46 excl x0
+
+    confirmationMsg: .asciz "Would you like to insert another value onto the grid (Y/n): " //60 excp x0
 
     userCoords: .fill 2,1,0
     userValue: .fill 100,1,0
